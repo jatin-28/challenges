d313ac25ec91ba9a6c64a8ace200f85b10345d08
jatin.shah <jatin.shah@gamesys.co.uk>
2018-09-16 07:05:36 +0100

    First working version

--- src/main/java/com/company/GuitarSubSetAttempts.java
+++ src/main/java/com/company/GuitarSubSetAttempts.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
@@ -41,6 +42,8 @@ public class GuitarSubSetAttempts {
 
     // second attempt
     public List<Integer> findGuitarSubListSecondAttempt(List<Integer> input, int totalSum) {
+        Collections.sort(input);
+        Collections.reverse(input);
         return addCombinationsSecondAttempt(input.get(0), input.subList(1, input.size()), totalSum);
     }
 
@@ -50,31 +53,26 @@ public class GuitarSubSetAttempts {
         if (totalSum - head >= 0) {
             outputList.add(head);
             totalSum -= head;
+            System.out.println("added==" + outputList);
         }
 
         if(totalSum == 0) {
-            System.out.println("first_totalSum output: " +outputList);
-            return outputList;
-        }
-
-        if( input.isEmpty()) {
-            System.out.println("first_isEmpty output: " +outputList);
-            return outputList;
-        }
-
-        if( input.size() == 1) {
+//            System.out.println("first_totalSum output: " +outputList);
+            //return outputList;
+        } else if( input.isEmpty()) {
+//            System.out.println("first_isEmpty output: " +outputList);
+            //return outputList;
+        } else if( input.size() == 1) {
             List<Integer> returnList = addCombinationsSecondAttempt(input.get(0), emptyList(), totalSum);
             outputList.addAll(returnList);
 
             //if(outputList.stream)
 
-        }
-
-        if( input.size() >= 2) {
+        } if( input.size() >= 2) {
             outputList.addAll(addCombinationsSecondAttempt(input.get(0), input.subList(1, input.size()), totalSum));
         }
 
-
+        System.out.println("output==" + outputList);
         return outputList;
     }
 
